e657ea58b1aeecd2c59c10eb1629ddf0974e596c
Matthew O. Smith <matt@m0smith.com>
2014-09-19 19:14:38 -0600

    Rename roll to roll-die and roll-dice for a single and multiple dice

--- src/crawl/dice.clj
+++ src/crawl/dice.clj
@@ -1,8 +1,15 @@
 (ns crawl.dice)
 
-(defn roll 
+(def default-sides 20)
+
+(defn roll-die 
   "Roll a single dice and get a number between 1 and the number of
-  sides passed.  The default sides is 20"
-  ([] (roll 20))
+  sides passed.  "
+  ([] (roll-die default-sides))
   ([sides] (inc (rand-int sides))))
   
+(defn roll-dice
+  "Roll the number of dice and return the results as a seq.  If sides is not passed, default-sides is used."
+  ([num] (roll-dice num default-sides))
+  ([num sides] (take num (repeatedly #(roll-die sides)))))
+		
