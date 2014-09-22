d698b1a7e7c3f9f0714255e3a61fc1813a7d2806
Matthew O. Smith <matt@m0smith.com>
2014-09-21 21:07:56 -0600

    Read the prototypes from a file.

--- src/crawl/monster.clj
+++ src/crawl/monster.clj
@@ -1,5 +1,6 @@
 (ns crawl.monster
-  (:require [crawl.monster.catalog :refer :all]))
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]))
 
 
 (defrecord MonstorPrototype [pid type ac max-hp attack-dice dammage-dice loot])
@@ -7,6 +8,12 @@
 (defrecord Monstor [id pid type ac max-hp hp attack-dice dammage-dice loot])
 
 
-(defn prototype-catalog []
-  (let [vals (map #(apply ->MonstorPrototype %) catalog-def)]
+(defn prototype-catalog 
+  "Create the catalog of monster prototypes."
+  []
+  (let [vals (->> "monsters.edn"
+                  io/resource
+                  slurp
+                  edn/read-string
+                  (map #(apply ->MonstorPrototype %)))]
     (zipmap (map :pid vals) vals)))
