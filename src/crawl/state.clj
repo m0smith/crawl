250d7371548a06fd8d264301c4d45472ccf03b25
Matthew O. Smith <matt@m0smith.com>
2014-09-25 09:03:06 -0600

    simplify

--- src/crawl/state.clj
+++ src/crawl/state.clj
@@ -2,5 +2,5 @@
 
 (defrecord GameState [context running mode combatants zoo adventurer] )
 
-(defn default-state [context adventurer]
+(defn default-start-state [context adventurer]
   (->GameState context true :move nil { (:id adventurer) adventurer } (:id adventurer)))
